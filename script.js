6e8bb5505b9308d1f3ff541fb0447a7e61458e05
approved4me <approved4me@gmail.com>
2022-12-11 16:11:12 +0100

    upate

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ let ratingNrs = document.getElementsByClassName("rating-li");
 for (let i = 0; i < ratingNrs.length; i++) {
     let ratingNr = ratingNrs[i];
     ratingNr.onclick = function() {
-        ratingNr.classList.toggle("active");
-        console.log(ratingNr.textContent);
+      ratingNr.classList.toggle("active");
+      console.log(ratingNr.textContent);
     }
-
-    }
+  }
