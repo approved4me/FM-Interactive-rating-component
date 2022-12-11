5ac479ad0800490e08e311fadfba0d109a284e17
approved4me <approved4me@gmail.com>
2022-12-11 18:01:58 +0100

    content rating and thanks update

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ for (let i = 0; i < ratingNrs.length; i++) {
     let ratingNr = ratingNrs[i];
     ratingNr.onclick = function() {
       ratingNr.classList.toggle("active");
-      console.log(ratingNr.textContent);
+      console.log(ratingNrs);
     }
   }
