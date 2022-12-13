b50f082614117c37ee739274740a47d2258f4c68
approved4me <approved4me@gmail.com>
2022-12-13 17:32:36 +0100

    upate rating to be shown in the thank you message

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const form = document.querySelector(".rating-form");
-const reteStart = document.querySelector(".content_rating");
+const rateStart = document.querySelector(".content_rating");
 const thankYou = document.querySelector(".content_thanks");
+const value = document.getElementById("rating-value");
 
 form.addEventListener("submit", function(e) {
   e.preventDefault(); //prevents Browser from refreshing
-  reteStart.classList.add("hidden");
+  const selector = "input[name=ratings]:checked";
+  const checkedInput = document.querySelector(selector);
+  rateStart.classList.add("hidden");
   thankYou.classList.remove("hidden");
+  value.innerHTML = checkedInput.value;
+  console.log(checkedInput.value);
 })
